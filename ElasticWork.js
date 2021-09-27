31b461871afc8d0b44cdfe7c9a427b8ae491a555
KOBEOMSEOK <rhrhrhqjatjr@naver.com>
2021-09-27 15:24:18 +0900

    Elastic Work Finish

---
+++ ElasticWork.js
@@ -0,0 +1,14 @@
+// import React, { useState } from "react";
+// import { AgGridReact } from "ag-grid-react";
+// import "ag-grid-community/dist/styles/ag-theme-balham.css";
+// import "ag-grid-community/dist/styles/ag-grid.css";
+
+// const ElasticWork = () => {
+//     return (
+//         <div>
+
+//         </div>
+//     )
+// }
+
+// export default ElasticWork;
